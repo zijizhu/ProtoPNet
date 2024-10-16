ac18433397c118740aafbdbac1c5bce848380bc8
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-10-17 00:16:28 +1100

    tweak

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-python3 main.py -gpuid='0' -base_architecture="dinov2_vitb_exp"
-python3 main.py -gpuid='0' -base_architecture="dinov2_vitb_exp"
+python3 main.py -gpuid='0' -base_architecture="dinov2_vits_exp"
+python3 main.py -gpuid='0' -base_architecture="dinov2_vits_exp"
