662d3260f375581ad9a49d926443e1e7fa04e804
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-10-24 12:08:09 +1100

    fix

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-python3 main.py -gpuid='0' -arch="dinov2_vits_exp" -num_prototypes=2000
-python3 main.py -gpuid='0' -arch="dinov2_vits_exp" -num_prototypes=1000
+python3 main.py -gpuid='0' -arch="dinov2_vitb_exp" -num_prototypes=2000
+python3 main.py -gpuid='0' -arch="dinov2_vitb_exp" -num_prototypes=1000
