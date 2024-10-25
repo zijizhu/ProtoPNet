986be63dc500e0e13aa425c8319b0bab12e1d940
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-10-25 18:28:32 +1100

    change model save dir

--- main.py
+++ main.py
@@ -39,7 +39,8 @@ if __name__ == "__main__":
 
     base_architecture_type = re.match('^[a-z]*', base_architecture).group(0)
 
-    model_dir = './saved_models/' + base_architecture + '/'
+    model_dir = os.path.join('saved_models', base_architecture, f"{args.num_prototypes}-prototypes")
+
     makedir(model_dir)
     shutil.copy(src=os.path.join(os.getcwd(), __file__), dst=model_dir)
     shutil.copy(src=os.path.join(os.getcwd(), 'settings.py'), dst=model_dir)
