398e7a73853e5e976daa93ce463857062c77398c
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-11-20 17:23:42 +1100

    update training

--- settings.py
+++ settings.py
@@ -32,8 +32,8 @@ coefs = {
     'l1': 1e-4,
 }
 
-num_train_epochs = 50
-num_warm_epochs = 10
+num_train_epochs = 80
+num_warm_epochs = 20
 
 push_start = 20
 # push_epochs = [i for i in range(num_train_epochs) if i % 10 == 0]
