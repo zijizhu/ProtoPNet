b13e148e28c5534bd43fe1c9ceb7df9d4c951a2c
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-11-20 23:15:48 +1100

    update

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 # python3 main.py -gpuid='0' -arch="dinov2_vitb_exp" -num_prototypes=980 -dataset='cars'
 # python3 main.py -gpuid='0' -arch="dinov2_vits_exp" -num_prototypes=980 -dataset='cars'
 python3 main.py -gpuid='0' -arch="dinov2_vitb_exp" -num_prototypes=600 -dataset='dogs'
-python3 main.py -gpuid='0' -arch="dinov2_vitb_exp" -num_prototypes=588 -dataset='cars'
-python3 main.py -gpuid='0' -arch="dinov2_vits_exp" -num_prototypes=588 -dataset='cars'
-
 python3 main.py -gpuid='0' -arch="dinov2_vits_exp" -num_prototypes=600 -dataset='dogs'
 python3 main.py -gpuid='0' -arch="dinov2_vitb_exp" -num_prototypes=360 -dataset='dogs'
 python3 main.py -gpuid='0' -arch="dinov2_vits_exp" -num_prototypes=360 -dataset='dogs'
+
+python3 main.py -gpuid='0' -arch="dinov2_vitb_exp" -num_prototypes=588 -dataset='cars'
+python3 main.py -gpuid='0' -arch="dinov2_vits_exp" -num_prototypes=588 -dataset='cars'
